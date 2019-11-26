ed43b361b46f7a429b4c73da457a21d009fcf973
devanshudave12 <54864466+devanshudave12@users.noreply.github.com>
2019-11-25 22:36:34 -0600

    updated

--- maingame.py
+++ maingame.py
@@ -1,12 +1,45 @@
-import sys
+# import sys
 # from tabulate import tabulate
 # import characteristics
-# import map
+import map
 # import inventory
 # import continuousgameplay
 
 
-actions = ["quit","map","inventory"]
-choice = input("whichS option would you like to go with: ")
 
-    map()
+
+def get_input_wish(message):
+    """Get user input and convert the string to lowercase"""
+    action_input = input(message)
+    return action_input.lower() 
+# def play():
+#     '''Return a menu in formated form''''
+action = ["map","inventory","characteristics","quit"]
+
+def options():
+    '''Return a full menu showing options'''
+    print(f"Which one would you like to open:")
+    for actions in action[:4]:
+        print('*** ' + actions.title())   # prints the options of location
+
+options()
+# while true:
+action_input = get_input_wish("Action: ")
+
+def  action_function():
+    '''input by the user in formatted form'''
+    if action_input == "map":
+        startgame()
+
+# def get_input_wish(message):
+#     """Get user input and convert the string to lowercase"""
+#     action_input = input(message)
+#     return action_input.lower()
+#
+# def choose_map():
+#     '''returns the printed map for the player'''
+#
+
+
+
+# choice = input("action:  ")
--- map.py
+++ map.py
@@ -1,29 +1,39 @@
+# Name - Devanshu Dave
+# course - Computer Science 30
+# period - 4
+# Date Created - 23/11/19
+# Description -  code shows the locations or prints the map of the game
+
 import random
 from tabulate import tabulate
 
 
+# defining the function to start game
 def startgame():
-    final = []
-    game = []
-    game1 = []
-    finalgame = []
+    '''print the map in formated and in the form of table'''
+    final = []   # making a list
+    game = []     # making a list
+    game1 = []    # list for game1
+    finalgame = []  # making list for final one whch will be main list
     f1 = []
-    i = 6
-    list = ['start', ['livingroom', 'bathroom', 'kitchen', 'restroom', "hall"]]
+    i = 6    # index is set equal to 6
+    # list for locations
+    list = ['start', ['livingroom', 'bathroom', 'kitchen', 'bedroom', "hall"]]
+    # using loop for printing
     for j in range(1):
-        final.append(list[0])
+        final.append(list[0])  # adding a value in a list
         for i in range(20):
-            final.append(random.choice(list[len(list)-1]))
-            # append it into final list
+            final.append(random.choice(list[len(list)-1]))  # adding it to list
+            # adding  it into final list
         game.append(final)
         final = []
     for k in game[0]:
         if k not in game1:
             game1.append(k)
-    finalgame.append(game1)
+    finalgame.append(game1)  # adding it to the main list
 
-    a = finalgame[0][:3]
-    b = finalgame[0][3:]
+    a = finalgame[0][:3]    # it will divide the output into to 2 rows
+    b = finalgame[0][3:]    # it will divide the output into to 2 rows
 
     # print()
     # print(a)
@@ -31,6 +41,6 @@ def startgame():
     c = []
     c.append(a)
     c.append(b)
-    print(tabulate(c, tablefmt="fancy_grid"))
+    print(tabulate(c, tablefmt="fancy_grid"))   # printing it in a table
 
 startgame()
