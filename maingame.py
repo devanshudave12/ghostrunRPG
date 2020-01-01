5c52d272026838ff86212081810c1060c07b6bc5
devanshudave12 <54864466+devanshudave12@users.noreply.github.com>
2019-12-31 22:45:53 -0600

    updated main game file

--- maingame.py
+++ maingame.py
@@ -6,30 +6,32 @@
 # Description - the program that works the main game
 
 
-# import sys
-# from tabulate import tabulate
-import characteristics   # import the file
-import map   # import the file
-import inventory  # import the file
-# import inventory
-# import continuousgameplay
+import characteristics   # import the file  # import the file
+import inventory
+import map2 # import the file
+import time
 
-
+print("WELCOME TO THE GAME : THE GHOST RUN")
+time.sleep(2)
+print("HOPE YOU WILL ENJOY THIS GAME")
+time.sleep(3)
+print("LETS START")
+time.sleep(3)
 # defines the function for taking input
 def get_input_wish(message):
     """Get user input and convert the string to lowercase"""
     action_input = input(message)
     return action_input.lower()
-# def play():
-#     '''Return a menu in formated form''''
-actions = ["map", "inventory", "characteristics", "quit"]   # list for the user
+
+actions = ["start", "inventory", "characteristics", "quit"]   # list
 
 
 # implements the input of the user
 def action_function(get_action):
     '''input by the user in formatted form'''
-    if get_action == "map":
-        map.startgame()   # called the function
+    if get_action == "start":
+        print("yor are entering the game")
+        map2.game1()   # called the function
     elif get_action == "inventory":
         inventory.start_inven()   # called the function
     elif get_action == "characteristics":
@@ -55,13 +57,3 @@ while True:
     action_input = get_input_wish("Action: ")
     if action_input:
         action_function(action_input)  # lopped and calling the final one s
-
-# def get_input_wish(message):
-#     """Get user input and convert the string to lowercase"""
-#     action_input = input(message)
-#     return action_input.lower()
-#
-# def choose_map():
-#     '''returns the printed map for the player'''
-#
-# choice = input("action:  ")
