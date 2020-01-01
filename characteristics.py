58cc9e4490469b9468eb1503f9ea5eaad567b717
devanshudave12 <54864466+devanshudave12@users.noreply.github.com>
2019-12-31 22:45:00 -0600

    updated characteristics file

--- characteristics.py
+++ characteristics.py
@@ -5,13 +5,14 @@
 # Description - this code prints the characteristics of the gamer
 
 # siple dictonary
-character_1 = {'gamer': 'good weapons', 'health points': '150',
-               'secret': 'help of friendly ghost'}
+class characteristics:
+    character_1 = {'gamer': 'good weapons', 'health points': '150',
+                   'secret': 'help of friendly ghost'}
 
+    def get_charac():
+        # printing values from the dictionary as a statement with loops.
+        print(f"The gamer has proper {character_1['gamer']} to win.")
+        print(f"The gamer Total {character_1['health points']} health points.")
+        print(f"The game has a secret which is {character_1['secret']}. ")
 
-def get_charac():
-	# printing values from the dictionary as a statement with loops.
-	print(f"The gamer has proper {character_1['gamer']} to win.")
-	print(f"The gamer has Total {character_1['health points']} health points.")
-
-	print(f"The game has a secret which is {character_1['secret']}. ")
+# characteristics()
